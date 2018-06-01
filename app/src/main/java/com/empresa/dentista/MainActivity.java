c7d7e51b3c0f526bb1493d64dd452666483d7b45
nashvent <anibal.ventura@ucsp.edu.pe>
2018-06-01 14:55:06 -0500

    converson website a apk

---
+++ app/src/main/java/com/empresa/dentista/MainActivity.java
@@ -0,0 +1,21 @@
+package com.empresa.dentista;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView myWebView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        myWebView = (WebView)findViewById(R.id.webView);
+        WebSettings webSettings = myWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        myWebView.loadUrl("http://192.168.0.9");
+        myWebView.setWebViewClient(new WebViewClient());
+    }
+}
